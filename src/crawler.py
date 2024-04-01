4c2ab18a2995d530f7ea7eeb1159495d59e5abc5
azazaurus <1maxovmax1@gmail.com>
2024-04-01 20:05:15 +0400

    save page links during crawling as well

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Optional, Set, Union
+from typing import Dict, List, Optional, Set, Union
 from urllib.parse import urldefrag, urljoin, urlparse
 
 from bs4 import BeautifulSoup
@@ -34,14 +34,14 @@ def main(
 	log.debug("Initializing page repository")
 	page_repository = RawPageRepository()
 	page_repository.delete_all()
-	outcome_links = []
 
+	page_ids: Dict[str, int] = {}
+	page_outcome_links: Dict[str, Set[str]] = {}
 	page_urls_to_download = deque([root_page_url])
 	seen_page_urls = {root_page_url}
-	downloaded_page_urls: Set[str] = set()
 	whitelisted_domains_set = set(whitelisted_domains) if whitelisted_domains is not None else None
 	blacklisted_urls_set: Set[str] = set(blacklisted_urls) if blacklisted_urls is not None else set()
-	while len(page_urls_to_download) > 0 and len(downloaded_page_urls) < max_pages_count:
+	while len(page_urls_to_download) > 0 and len(page_ids) < max_pages_count:
 		page_url = page_urls_to_download.popleft()
 		log.info("Downloading " + page_url)
 		page_markup = _download(page_url, seen_page_urls)
@@ -62,24 +62,24 @@ def main(
 			continue
 
 		if count_words(get_text(parsed_page)) >= min_words_per_page_count:
-			downloaded_page_urls.add(page_url)
-	
 			log.debug("Saving " + page_url)
-			page = NewRawPage(page_url, str(page_markup))
+			page_to_create = NewRawPage(page_url, str(page_markup))
 			try:
-				page_repository.create(page)
+				page = page_repository.create(page_to_create)
+				page_ids[page.url] = page.id_
 			except Exception as exception:
 				log.error(
 					f"Unable to save {page_url}:" + os.linesep + format_exception(exception))
 	
 		child_urls = set(_get_link_urls(page_url, parsed_page, whitelisted_domains_set, blacklisted_urls_set))
-		outcome_links.append(child_urls)
-		child_urls = child_urls.difference(seen_page_urls)
+		if len(child_urls) > 0:
+			page_outcome_links[page_url] = child_urls
 
+		child_urls = child_urls.difference(seen_page_urls)
 		page_urls_to_download.extend(child_urls)
 		seen_page_urls = seen_page_urls.union(child_urls)
 
-	return outcome_links
+	_save_page_outcome_links(page_ids, page_outcome_links)
 
 
 def _download(url: str, seen_page_urls: Set[str]) -> Union[str, Exception, None]:
@@ -141,6 +141,29 @@ def get_text(markup: BeautifulSoup) -> str:
 
 def count_words(text: str) -> int:
 	return len(text.split())
+
+
+def _save_page_outcome_links(page_ids: Dict[str, int], page_outcome_links: Dict[str, Set[str]]) -> None:
+	with open("raw-pages/links.txt", "w", encoding = "utf-8") as links_file:
+		for page_url, links in page_outcome_links.items():
+			if page_url not in page_ids:
+				continue
+				
+			links_line_ids = [page_ids[page_url]]
+			links_line_ids += [page_ids[link] for link in filter(lambda link: link in page_ids, links)]
+			links_file.write(" ".join(str(link_id) for link_id in links_line_ids) + "\n")
+
+
+def read_page_outcome_links() -> Dict[int, List[int]]:
+	links: Dict[int, List[int]] = {}
+	with open("raw-pages/links.txt", "r", encoding = "utf-8") as links_file:
+		line = links_file.readline().rstrip()
+		while line:
+			line_ids = [int(id_) for id_ in line.split()]
+			links[line_ids[0]] = line_ids[1:]
+			line = links_file.readline()
+
+	return links
 
 
 if __name__ == "__main__":
